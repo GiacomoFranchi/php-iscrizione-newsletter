bc079a5b2358bef1135294ca4c41a124fff1bece
GiacomoFranchi <giacomo.franchi.97@gmail.com>
2023-12-13 16:33:11 +0100

    start: open new file

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Iscrizione Newsletter</title>
+</head>
+
+<body>
+    <section>
+        <h1>Iscrizione Newsletter</h1>
+        <form action="index.php" method="GET">
+            <div>
+                <label for="mail">Inserisci la tua mail per registrarti</label>
+                <input name="mail" type="text" placeholder="esempio@es.es">
+            </div>
+            <div>
+                <button type="submit">Invia</button>
+            </div>
+        </form>
+    </section>    
+</body>
+</html>
